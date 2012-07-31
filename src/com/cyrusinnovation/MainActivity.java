9b8cdadd3e7f2300dc487159116d50fd69dc470d
Cyrus Innovation <team-telvue@cyrusinnovation.com>
2012-07-31 16:35:14 -0400

    first commit

---
+++ src/com/cyrusinnovation/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cyrusinnovation;
+
+import android.os.Bundle;
+import org.apache.cordova.DroidGap;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+}
